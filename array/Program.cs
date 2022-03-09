370745aa268ce74f202de7be097c251176abae70
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-09 10:54:12 +0400

    commit

---
+++ array/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region //ARRAY
+            int[] arr = { 41, 25, 26, 48 };
+            Min(arr);
+            Console.WriteLine(Min(arr));
+        }
+        static int Min(int[] arr)
+        {
+            int Min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < Min)
+                {
+                    Min = arr[i];
+                }
+            }
+            return Min;
+            #endregion
+
+
+        }
+        #region EDEDIN reqemlerinin sayi
+
+
+        static void Main(string[] args)
+        {
+            int num = 556;
+            int count = 0;
+            Console.WriteLine(count);
+
+
+        }
+
+
+        private static int NumLength(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                num = num / 10;
+                count++;
+
+            }
+            return count;
+
+        }
+
+
+    }
+ }
